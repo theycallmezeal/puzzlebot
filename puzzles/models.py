e61637e569915a2337564325f51d526041fa7311
Adam <zealan@gmail.com>
2020-08-08 01:26:22 -0400

    store highest solved tier in model

--- puzzles/models.py
+++ puzzles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Tier(models.Model):
 	number = models.PositiveSmallIntegerField(unique=True)
@@ -13,3 +15,18 @@ class Puzzle(models.Model):
 class SolvedPuzzle(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+	
+class HighestSolvedTier(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
+	
+# link up the HighestSolvedTier class to the User class; update HighestSolvedTier whenever its User is updated as well.
+@receiver(post_save, sender=User)
+def create_highest_solved_tier(sender, instance, created, **kwargs):
+	if created:
+		HighestSolvedTier.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_highest_solved_tier(sender, instance, **kwargs):
+	# User.highest_solved_tier is an automatically generated field.
+	instance.highest_solved_tier.save()
