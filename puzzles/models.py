7153e76fbb8de228c3404819059bf2ad0970a7cb
Adam <zealan@gmail.com>
2020-08-08 16:27:16 -0400

    commit login templates and diddle with admin site

--- puzzles/models.py
+++ puzzles/models.py
@@ -6,15 +6,24 @@ from django.dispatch import receiver
 class Tier(models.Model):
 	number = models.PositiveSmallIntegerField(unique=True)
 	num_to_unlock = models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return 'Tier ' + str(self.number) + ' (' + str(self.num_to_unlock) + ' to unlock)'
 
 class Puzzle(models.Model):
 	number = models.PositiveSmallIntegerField(unique=True)
 	tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
 	answer = models.CharField(max_length=100)
+	
+	def __str__(self):
+		return 'Puzzle ' + str(self.number) + ' (Tier ' + str(self.tier.number) + '; answer: ' + self.answer + ')'
 
 class SolvedPuzzle(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.user.username + " solved puzzle " + str(self.puzzle.number)
 	
 	class Meta:
 		unique_together = ('user', 'puzzle')
