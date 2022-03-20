fb1a96b6131bd21082a5fb5eeb8d01a54968dc58
Jacob Adams <jacob@tookmund.com>
2022-03-20 16:25:34 -0400

    Use DATABASE_URL for persisent configuration
    Can't rely on sqlite on Heroku

--- puzzlebot/settings.py
+++ puzzlebot/settings.py
@@ -1,9 +1,9 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -65,13 +65,9 @@ WSGI_APPLICATION = 'puzzlebot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES['default'] = dj_database_url.config(
+        default='sqlite://' + BASE_DIR + '/db.sqlite3',
+        conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
