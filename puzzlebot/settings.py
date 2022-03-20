c4c0a739e88edab99b5ec501c39560963fd6ee53
Jacob Adams <jacob@tookmund.com>
2022-03-20 16:28:26 -0400

    BASE_DIR is a Python Path object, which requires a slash
    Operator overloading strikes again!

--- puzzlebot/settings.py
+++ puzzlebot/settings.py
@@ -66,7 +66,7 @@ WSGI_APPLICATION = 'puzzlebot.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES['default'] = dj_database_url.config(
-        default='sqlite://' + BASE_DIR + '/db.sqlite3',
+        default='sqlite:/' / BASE_DIR / 'db.sqlite3',
         conn_max_age=600, ssl_require=True)
 
 # Password validation
